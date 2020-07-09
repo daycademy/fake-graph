ed100ee4758593465653a75abf9b56d1702556b4
florianwoelki <florianwoelki@gmx.de>
2020-07-09 13:14:16 +0200

    refactor: add setup util

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { UserResolver } from './resolvers/UserResolver';
+import { insertData } from './util/setup-util';
 
 (async () => {
   const app = express();
+
+  insertData();
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
---
+++ src/util/setup-util.ts
@@ -0,0 +1,17 @@
+import faker from 'faker';
+import { User } from '../entity/User';
+
+export const users: Array<User> = [];
+
+export function insertData(): void {
+  for (let i = 0; i < 10; i += 1) {
+    users.push(
+      {
+        id: i,
+        age: faker.random.number(100),
+        fullname: faker.name.findName(),
+        username: faker.internet.userName(),
+      },
+    );
+  }
+}
