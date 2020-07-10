8ab3905bcfa945fd779c9c2ea7eeb4a92498692a
florianwoelki <florianwoelki@gmx.de>
2020-07-10 08:47:56 +0200

    feat(resolver): add middleware rateLimit

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,17 +1,20 @@
 import {
-  Query, Resolver, FieldResolver, Ctx, Root,
+  Query, Resolver, FieldResolver, Ctx, Root, UseMiddleware,
 } from 'type-graphql';
 import { User } from '../entity/User';
 import { MyContext } from '../MyContext';
+import { rateLimit } from '../middleware/rateLimit';
 
 @Resolver(() => User)
 export class UserResolver {
   @Query(() => [User])
+  @UseMiddleware(rateLimit)
   users(): Promise<User[]> {
     return User.find();
   }
 
   @FieldResolver()
+  @UseMiddleware(rateLimit)
   posts(@Ctx() { loaders }: MyContext, @Root() user: User) {
     return loaders.postLoader.load(user.id);
   }
