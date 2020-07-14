7d57d7c4edff9a79b0dc2e261fadc4736b5dd0d0
FlorianWoelki <FlorianWoelki@gmx.de>
2020-07-14 17:26:04 +0200

    feat(resolver): add video field resolver to post resolver

--- src/resolvers/PostResolver.ts
+++ src/resolvers/PostResolver.ts
@@ -1,10 +1,11 @@
 import {
-  Resolver, Query, UseMiddleware, Arg, Int,
+  Resolver, Query, UseMiddleware, Arg, Int, FieldResolver, Ctx, Root,
 } from 'type-graphql';
 import { Post } from '../entity/Post';
 import { rateLimit } from '../middleware/rateLimit';
+import { MyContext } from '../MyContext';
 
-@Resolver()
+@Resolver(() => Post)
 export class PostResolver {
   @Query(() => [Post])
   @UseMiddleware(rateLimit)
@@ -16,5 +17,11 @@ export class PostResolver {
   @UseMiddleware(rateLimit)
   async post(@Arg('id', () => Int) id: number): Promise<Post | undefined> {
     return Post.findOne({ where: { id } });
+  }
+
+  @FieldResolver()
+  @UseMiddleware(rateLimit)
+  video(@Ctx() { loaders }: MyContext, @Root() post: Post) {
+    return loaders.videoLoader.load(post.id);
   }
 }
