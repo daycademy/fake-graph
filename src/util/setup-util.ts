b259d41ea5141347bace96e8558b0202be9e8e1c
florianwoelki <florianwoelki@gmx.de>
2020-07-16 20:15:59 +0200

    feat: add UserRole value to setup

--- src/util/setup-util.ts
+++ src/util/setup-util.ts
@@ -2,6 +2,7 @@ import faker from 'faker';
 import { User } from '../entity/User';
 import { Post } from '../entity/Post';
 import Video from '../entity/Video';
+import { UserRole } from '../entity/enums/UserRole';
 
 const ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -9,6 +10,9 @@ export function insertData(): void {
   ids.forEach(async (id) => {
     await User.insert({
       id,
+      role: faker.random.number(100) % 2 === 0
+        ? UserRole.ADMIN.toString()
+        : UserRole.USER.toString(),
       age: faker.random.number(100),
       fullname: faker.name.findName(),
       username: faker.internet.userName(),
