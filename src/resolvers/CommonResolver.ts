bc5601731cbb4dc69156d7dbbf498f306101b595
florianwoelki <florianwoelki@gmx.de>
2020-07-19 20:04:30 +0200

    feat(resolver): add rate limit to common resolver

--- src/resolvers/CommonResolver.ts
+++ src/resolvers/CommonResolver.ts
@@ -1,13 +1,15 @@
 import {
-  Resolver, Query, Arg, ID,
+  Resolver, Query, Arg, ID, UseMiddleware,
 } from 'type-graphql';
 import { SearchResultUnion } from '../entity/unions/SearchResult';
 import { Post } from '../entity/Post';
 import { User } from '../entity/User';
+import { rateLimit } from '../middleware/rateLimit';
 
 @Resolver()
 export class CommonResolver {
   @Query(() => [SearchResultUnion])
+  @UseMiddleware(rateLimit)
   async search(@Arg('id', () => ID) id: number): Promise<Array<Post | User>> {
     const posts = await Post.find({ where: { id } });
     const users = await User.find({ where: { id } });
