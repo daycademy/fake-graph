e7fb618e295a59c01645de8a4162f6abd9da5ebb
florianwoelki <florianwoelki@gmx.de>
2020-07-19 20:07:38 +0200

    feat(resolver): add id list modifier

--- src/resolvers/CommonResolver.ts
+++ src/resolvers/CommonResolver.ts
@@ -1,6 +1,7 @@
 import {
   Resolver, Query, Arg, ID, UseMiddleware,
 } from 'type-graphql';
+import { In } from 'typeorm';
 import { SearchResultUnion } from '../entity/unions/SearchResult';
 import { Post } from '../entity/Post';
 import { User } from '../entity/User';
@@ -10,9 +11,9 @@ import { rateLimit } from '../middleware/rateLimit';
 export class CommonResolver {
   @Query(() => [SearchResultUnion])
   @UseMiddleware(rateLimit)
-  async search(@Arg('id', () => ID) id: number): Promise<Array<Post | User>> {
-    const posts = await Post.find({ where: { id } });
-    const users = await User.find({ where: { id } });
+  async search(@Arg('id', () => [ID]) ids: [number]): Promise<Array<Post | User>> {
+    const posts = await Post.find({ where: { id: In(ids) } });
+    const users = await User.find({ where: { id: In(ids) } });
     return [...posts, ...users];
   }
 }
